c25e1005222384e503a717446d41b393792653d6
seansql <shawnq2116@gmal.com>
2020-09-22 19:03:25 -0400

    Add project files.

---
+++ GeneralStore.MVC/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using GeneralStore.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStore.MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        // Add the application Db Context (link to the datebase)
+        private ApplicationDbContext _db = new ApplicationDbContext();
+
+        // GET: Product/Index
+        public ActionResult Index()
+        {
+            // See below (modifying ApplicationDbContext class)
+            return View(_db.Products.ToList());
+        }
+
+        
+
+
+    }
+}
