222bb5de947f9140814c9688ca5fcfa7906bd5b9
seansql <shawnq2116@gmal.com>
2020-09-23 11:39:42 -0400

    Built out customer add and delete functionality

---
+++ GeneralStore.MVC/Controllers/CustomerController.cs
@@ -0,0 +1,59 @@
+﻿using GeneralStore.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStore.MVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // Add application dbcontext aka link to database
+        private ApplicationDbContext _db = new ApplicationDbContext();
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View(_db.Customers.ToList());
+        }
+
+        // GET: Customer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customer/Create
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Customers.Add(customer);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+
+        // GET: Delete
+        // Customer/Delete/{id}
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Customer customer = _db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+
+
+    }
+}
--- GeneralStore.MVC/Controllers/ProductController.cs
+++ GeneralStore.MVC/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using GeneralStore.MVC.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -15,8 +16,10 @@ namespace GeneralStore.MVC.Controllers
         // GET: Product/Index
         public ActionResult Index()
         {
+            List<Product> productList = _db.Products.ToList();
+            List<Product> orderedList = productList.OrderBy(prod => prod.Name).ToList();
             // See below (modifying ApplicationDbContext class)
-            return View(_db.Products.ToList());
+            return View(orderedList);
         }
 
         // GET: Product/Create
@@ -54,6 +57,9 @@ namespace GeneralStore.MVC.Controllers
             return View(product);
         }
 
+
+        // POST: Delete
+        // Product/Delete/{id}
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult Delete(int id)
@@ -62,9 +68,53 @@ namespace GeneralStore.MVC.Controllers
             _db.Products.Remove(product);
             _db.SaveChanges();
             return RedirectToAction("Index");
-
         }
 
+        // GET: Edit
+        // Product/Edit/{id}
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Product product = _db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Edit// Product/Edit/{id}
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(product).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        // GET : Details
+        // Product/Details/{id}
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Product product = _db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
 
 
     }
